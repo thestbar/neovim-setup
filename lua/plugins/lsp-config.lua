0a87db588dd341a65d3d3f50d72d37d9647d2745
thestbar <stavrosbarousis@gmail.com>
2024-02-18 17:13:56 +0200

    Add LSP configuration

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls", "tsserver", "rubocop", "ruby_ls", "solargraph",
+          "sorbet", "stimulus_ls",
+        }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.tsserver.setup({})
+      lspconfig.rubocop.setup({})
+      lspconfig.ruby_ls.setup({})
+      lspconfig.solargraph.setup({})
+      lspconfig.sorbet.setup({})
+      lspconfig.stimulus_ls.setup({})
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+
+    end
+  }
+}
