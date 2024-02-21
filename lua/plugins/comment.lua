631cd2e06ee7828a70eb2b653f050e8cb207650c
Stavros <sbarousis@skroutz.gr>
2024-02-21 14:21:26 +0200

    Add comment plugin

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,12 @@
+return {
+	"numToStr/Comment.nvim",
+	keys = {
+		{ "<C-_>", "gcc", mode = "n", remap = true },
+		{ "<C-/>", "gcc", mode = "n", remap = true },
+		{ "<C-_>", "gcgv", mode = "v", remap = true },
+		{ "<C-/>", "gcgv", mode = "v", remap = true },
+	},
+	config = function()
+		require("Comment").setup()
+	end,
+}
