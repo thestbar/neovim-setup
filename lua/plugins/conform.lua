e692f9939b49591ac5dd9969cfd3ef117e1ceb3e
Stavros <sbarousis@skroutz.gr>
2024-02-21 14:15:30 +0200

    Add conform plugin

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,24 @@
+return {
+	"stevearc/conform.nvim",
+	dependencies = { "mason.nvim" },
+	lazy = true,
+	cmd = "ConformInfo",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			javascript = { { "prettierd", "prettier" } },
+			javascriptreact = { { "prettierd", "prettier" } },
+			css = { { "prettierd", "prettier" } },
+			scss = { { "prettierd", "prettier" } },
+		},
+	},
+	keys = {
+		{
+			"<leader>ft",
+			function()
+				require("conform").format({ lsp_fallback = true })
+			end,
+			mode = "n",
+		},
+	},
+}
