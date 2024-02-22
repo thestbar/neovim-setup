9c6a7acc7cf929ca54728bc05a20ee297c80b5ec
Stavros <sbarousis@skroutz.gr>
2024-02-22 16:30:46 +0200

    Add native fzf for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,11 +2,25 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
     config = function()
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+      require("telescope").setup({
+        extensions = {
+          fzf_native = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+          },
+        },
+      })
     end,
   },
   {
