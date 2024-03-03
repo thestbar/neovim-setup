9c5da5305b6c23d49f6b57e63d9ce7f39b2a067c
Stavros <sbarousis@skroutz.gr>
2024-03-03 13:56:19 +0200

    Configure dashboard and `alpha` plugin

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -7,6 +7,14 @@ return {
     local alpha = require("alpha")
     local dashboard = require("alpha.themes.dashboard")
 
+    dashboard.section.buttons.val = {
+      dashboard.button("e", " New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("f", "󰈞 Find file", ":Telescope find_files <CR>"),
+      dashboard.button("h", "󰋚 Recently used files", ":Telescope oldfiles <CR>"),
+      dashboard.button("g", " Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("q", "󰅝 Quit Neovim", ":qa<CR>"),
+    }
+
     dashboard.section.header.val = {
       [[                                                                       ]],
       [[                                                                       ]],
