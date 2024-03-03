1586fda56272067ed519995853dc55b6799c3175
Stavros <sbarousis@skroutz.gr>
2024-03-03 13:44:51 +0200

    Bind old files search `telescope` shortcut

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,6 +11,7 @@ return {
       vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+      vim.keymap.set("n", "<leader>fh", builtin.oldfiles, {})
       require("telescope").setup({
         extensions = {
           fzf_native = {
