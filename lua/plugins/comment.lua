57402e6549c1619cceff4510fb31654a59a05cbf
Stavros <sbarousis@skroutz.gr>
2024-03-03 13:21:39 +0200

    Reorganize keybindings for `comment` plugin

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,12 +1,10 @@
 return {
-	"numToStr/Comment.nvim",
-	keys = {
-		{ "<C-_>", "gcc", mode = "n", remap = true },
-		{ "<C-/>", "gcc", mode = "n", remap = true },
-		{ "<C-_>", "gcgv", mode = "v", remap = true },
-		{ "<C-/>", "gcgv", mode = "v", remap = true },
-	},
-	config = function()
-		require("Comment").setup()
-	end,
+  "numToStr/Comment.nvim",
+  keys = {
+    { "<C-/>", "gcc", mode = "n", remap = true },
+    { "<C-/>", "gcgv", mode = "v", remap = true },
+  },
+  config = function()
+    require("Comment").setup()
+  end,
 }
