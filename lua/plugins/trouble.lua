9e21b1211cef31180155009a4c69662d0165d3e0
Stavros <sbarousis@skroutz.gr>
2024-03-03 13:25:47 +0200

    Format `trouble.lua` file

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,9 +1,9 @@
 return {
-	"folke/trouble.nvim",
-	cmd = { "TroubleToggle", "Trouble" },
-	opts = { use_diagnostic_signs = true },
-	keys = {
-		{ "<leader>df", "<cmd>TroubleToggle document_diagnostics<cr>" },
-		{ "<leader>dw", "<cmd>TroubleToggle workspace_diagnostics<cr>" },
-	},
+  "folke/trouble.nvim",
+  cmd = { "TroubleToggle", "Trouble" },
+  opts = { use_diagnostic_signs = true },
+  keys = {
+    { "<leader>df", "<cmd>TroubleToggle document_diagnostics<cr>" },
+    { "<leader>dw", "<cmd>TroubleToggle workspace_diagnostics<cr>" },
+  },
 }
