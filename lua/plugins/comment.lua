8e26c462d10838c902aff51596b08a58028f3a5f
Stavros <sbarousis@skroutz.gr>
2024-04-09 16:22:29 +0300

    Remove non working key binding on `Comment` plugin

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,9 +1,5 @@
 return {
   "numToStr/Comment.nvim",
-  keys = {
-    { "<C-/>", "gcc", mode = "n", remap = true },
-    { "<C-/>", "gcgv", mode = "v", remap = true },
-  },
   lazy = false,
   config = function()
     require("Comment").setup()
