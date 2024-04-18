9dbf65cf78be91647cb884e8621d6608a7f8d241
Stavros <sbarousis@skroutz.gr>
2024-04-18 15:21:51 +0300

    Fix key bindings for `Comment` plugin
    We have to enable `<C-_>` key binding as well, because terminal
    has a problem and sends `<C-/>` as `<C-_>`.
    For more information see: https://github.com/vim/vim/issues/6191,
    https://apple.stackexchange.com/questions/24261/how-do-i-send-c-that-is-control-slash-to-the-terminal

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,5 +1,11 @@
 return {
   "numToStr/Comment.nvim",
+  keys = {
+    { "<C-_>", "gcc", mode = "n", remap = true },
+    { "<C-/>", "gcc", mode = "n", remap = true },
+    { "<C-_>", "gcgv", mode = "v", remap = true },
+    { "<C-/>", "gcgv", mode = "v", remap = true },
+  },
   lazy = false,
   config = function()
     require("Comment").setup()
