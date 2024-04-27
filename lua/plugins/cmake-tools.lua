0777cf194f2a8300f809d1b3d004a427c3cdc0f8
thestbar <stavrosbarousis@gmail.com>
2024-04-27 19:35:54 +0300

    Set focus to terminal when cmake run is executed
    This forces the cursor to always be at the bottom of the terminal.
    Therefore you are able to see the latest terminal udpates

--- lua/plugins/cmake-tools.lua
+++ lua/plugins/cmake-tools.lua
@@ -1,6 +1,14 @@
 return {
   "Civitasv/cmake-tools.nvim",
   config = function()
-    require("cmake-tools").setup({})
+    require("cmake-tools").setup({
+      cmake_runner = {
+        default_opts = {
+          terminal = {
+            focus = true,
+          },
+        },
+      },
+    })
   end,
 }
