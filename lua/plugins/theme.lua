c6b623badbcfdc9aa655874be12ffad4f191c44e
thestbar <stavrosbarousis@gmail.com>
2024-05-04 16:03:32 +0300

    Rename `catppuccin.lua` file to `theme.lua`

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,6 +2,7 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
+  lazy = false,
   config = function()
     require("catppuccin").setup({ flavour = "macchiato" })
     vim.cmd([[colorscheme catppuccin]])
