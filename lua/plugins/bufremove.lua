15861ce58ca993ac79f7750e56bab37242f98a8a
Stavros Barousis <sbarousis@skroutz.gr>
2024-05-14 11:00:57 +0300

    Add `bufremove` plugin
    With this plugin the functionality to close buffers is added to neovim.
    The binding CTRL + W is added to close a buffer (in case the buffer was
    edited by user then a confirmation panel will be displayed on the
    screen). Also the binding CTRL + A is added to force close a buffer
    (without asking for confirmation).

---
+++ lua/plugins/bufremove.lua
@@ -0,0 +1,32 @@
+return {
+  "echasnovski/mini.bufremove",
+  keys = {
+    {
+      "<C-w>",
+      function()
+        local bd = require("mini.bufremove").delete
+
+        if vim.bo.modified then
+          local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
+
+          -- Yes
+          if choice == 1 then
+            vim.cmd.write()
+            bd(0)
+            -- No
+          elseif choice == 2 then
+            bd(0, true)
+          end
+        else
+          bd(0)
+        end
+      end,
+    },
+    {
+      "<C-a>",
+      function()
+        require("mini.bufremove").delete(0, true)
+      end,
+    },
+  },
+}
