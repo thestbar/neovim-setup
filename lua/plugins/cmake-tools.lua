af2f76bd7edec30ed4c86a36323c345cfc9e1050
thestbar <stavrosbarousis@gmail.com>
2024-05-18 20:17:02 +0300

    Revert "Set focus to terminal when cmake run is executed"
    This reverts commit 4de4c264340b718e60630b4d0e95f5ad43dbb031. Commit
    is reverted because it introduced a new issue to the cmake-tools
    plugin. After executing the program, if the neovim terminal emulator
    was being closed, then when the program was built and run again, then
    cmake-tools was failing to execute it.

--- lua/plugins/cmake-tools.lua
+++ lua/plugins/cmake-tools.lua
@@ -1,14 +1,6 @@
 return {
   "Civitasv/cmake-tools.nvim",
   config = function()
-    require("cmake-tools").setup({
-      cmake_runner = {
-        default_opts = {
-          terminal = {
-            focus = true,
-          },
-        },
-      },
-    })
+    require("cmake-tools").setup({})
   end,
 }
