49a2aeb7613add5a267a81615107791097bec201
Stavros Barousis <sbarousis@skroutz.gr>
2024-06-04 13:10:14 +0300

    Replace `live_grep` with `live_grep_args`

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -11,7 +11,7 @@ return {
       dashboard.button("e", " New file", ":ene <BAR> startinsert <CR>"),
       dashboard.button("f", "󰈞 Find file", ":Telescope find_files <CR>"),
       dashboard.button("h", "󰋚 Recently used files", ":Telescope oldfiles <CR>"),
-      dashboard.button("g", " Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("g", " Find text", ":Telescope live_grep_args <CR>"),
       dashboard.button("q", "󰅝 Quit Neovim", ":qa<CR>"),
     }
 
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,11 +5,12 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      "nvim-telescope/telescope-live-grep-args.nvim",
     },
     config = function()
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", {})
       vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
       vim.keymap.set("n", "<leader>fh", builtin.oldfiles, {})
       vim.keymap.set("n", "<leader>fp", builtin.pickers, {})
@@ -28,6 +29,9 @@ return {
           theme = "dropdown",
         }
       end
+
+      local actions = require("telescope.actions")
+      local lga_actions = require("telescope-live-grep-args.actions")
 
       require("telescope").setup({
         defaults = {
@@ -55,6 +59,16 @@ return {
           pickers = default("Pickers"),
         },
         extensions = {
+          live_grep_args = {
+            auto_quoting = true,
+            mappings = {
+              i = {
+                ["<C-h>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+                ["<C-l>"] = lga_actions.quote_prompt({ postfix = " --iglob '!*spec' " }),
+                ["<C-o>"] = actions.to_fuzzy_refine,
+              },
+            },
+          },
           fzf_native = {
             fuzzy = true,
             override_generic_sorter = true,
@@ -63,6 +77,8 @@ return {
           },
         },
       })
+
+      require("telescope").load_extension("live_grep_args")
     end,
   },
   {
