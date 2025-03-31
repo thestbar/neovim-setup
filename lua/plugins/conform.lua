e9abec857e28d0fd3f5502553d749e3ec62385c6
Stavros Barousis <sbarousis@skroutz.gr>
2025-03-31 17:20:58 +0300

    Use `stop_after_first` on conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,10 +6,19 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
-      javascript = { { "prettierd", "prettier" } },
-      javascriptreact = { { "prettierd", "prettier" } },
-      css = { { "prettierd", "prettier" } },
-      scss = { { "prettierd", "prettier" } },
+      javascript = { "prettierd", "prettier" },
+      javascriptreact = { "prettierd", "prettier" },
+      css = { "prettierd", "prettier" },
+      scss = { "prettierd", "prettier" },
+    },
+    -- Add this option to make it stop after the first formatter succeeds
+    format_on_save = {
+      lsp_fallback = true,
+      timeout_ms = 500,
+    },
+    -- This option tells conform to try formatters in order until one succeeds
+    format = {
+      stop_after_first = true,
     },
   },
   keys = {
