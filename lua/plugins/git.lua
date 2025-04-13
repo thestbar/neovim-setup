566436918215fa7cbde5d6b4cc6015fd7ec099a5
thestbar <stavrosbarousis@gmail.com>
2025-04-14 00:10:44 +0300

    Uninstall vgit plugin

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -6,34 +6,6 @@ return {
     end,
   },
   {
-    "thestbar/vgit.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    keys = {
-      { "<leader>gd", ":VGit buffer_diff_preview<CR>", mode = "n" },
-      { "<leader>gh", ":VGit buffer_history_preview<CR>", mode = "n" },
-      { "<leader>gn", ":VGit hunk_down<CR>", mode = "n" },
-      { "<leader>gp", ":VGit hunk_up<CR>", mode = "n" },
-    },
-    config = function()
-      require("vgit").setup({
-        settings = {
-          -- Use gitsigns for blame
-          live_blame = {
-            enabled = false,
-          },
-          authorship_code_lens = {
-            enabled = false,
-          },
-          scene = {
-            diff_preference = "split",
-          },
-        },
-      })
-    end,
-  },
-  {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup({
