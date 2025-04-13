989171a7fb936696223d89ce20e16568cc125458
thestbar <stavrosbarousis@gmail.com>
2025-04-14 00:47:12 +0300

    Uninstall git-blame plugin

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -21,15 +21,4 @@ return {
       vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
     end,
   },
-  {
-    "f-person/git-blame.nvim",
-    dependencies = { "nvim-lualine/lualine.nvim" },
-    keys = {
-      { "<LEADER>gu", "<CMD>GitBlameOpenCommitURL<CR>", mode = "n" },
-    },
-    config = function()
-      vim.g.gitblame_display_virtual_text = 0
-      vim.g.gitblame_date_format = "%r"
-    end,
-  },
 }
