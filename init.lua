bee3c64d685084fdf70159cf2696e4369a60dac3
Stavros Barousis <sbarousis@skroutz.gr>
2026-06-02 11:22:46 +0300

    Disable ruby provider for neovim

--- init.lua
+++ init.lua
@@ -15,6 +15,7 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_node_provider = 0
+vim.g.loaded_ruby_provider = 0
 
 vim.filetype.add({
   extension = {
